d677d8d35a3f774c75db970068ad5478d1f1835b
李小帅 <592453675@qq.com>
2024-09-02 01:00:08 +0800

    no message

--- src/index.ts
+++ src/index.ts
@@ -57,16 +57,16 @@ class Schema {
   constructor(descriptor: Rules) {
     this.define(descriptor);
   }
-/**
- * 定义验证规则的方法
- * 
- * 本方法用于接收一组规则，并将其配置到当前的模式对象中
- * 如果传入的规则不符合预期格式或类型，将抛出错误
- * 
- * @param rules 验证规则对象，必须为非空对象
- * @throws {Error} 如果未传入规则或规则不是对象，则抛出错误
- * @throws {Error} 如果规则是以数组形式存在，则抛出错误
- */
+  /**
+   * 定义验证规则的方法
+   * 
+   * 本方法用于接收一组规则，并将其配置到当前的模式对象中
+   * 如果传入的规则不符合预期格式或类型，将抛出错误
+   * 
+   * @param rules 验证规则对象，必须为非空对象
+   * @throws {Error} 如果未传入规则或规则不是对象，则抛出错误
+   * @throws {Error} 如果规则是以数组形式存在，则抛出错误
+   */
   define(rules: Rules) {
     if (!rules) {
       throw new Error('Cannot configure a schema with no rules');
@@ -81,15 +81,15 @@ class Schema {
       this.rules[name] = Array.isArray(item) ? item : [item];
     });
   }
-/**
- * 设置或获取验证消息
- * 
- * 此方法用于在验证过程中定制化错误提示信息它允许从外部传入一组消息来覆盖默认的验证错误信息
- * 如果没有传入参数，则返回当前的消息配置
- * 
- * @param messages 可选参数，用于传入自定义的验证错误消息
- * @returns 如果传入参数，则返回更新后的消息配置；如果没有传入参数，则返回当前的消息配置
- */
+  /**
+   * 设置或获取验证消息
+   * 
+   * 此方法用于在验证过程中定制化错误提示信息它允许从外部传入一组消息来覆盖默认的验证错误信息
+   * 如果没有传入参数，则返回当前的消息配置
+   * 
+   * @param messages 可选参数，用于传入自定义的验证错误消息
+   * @returns 如果传入参数，则返回更新后的消息配置；如果没有传入参数，则返回当前的消息配置
+   */
   messages(messages?: ValidateMessages) {
     if (messages) {
       this._messages = deepMerge(newMessages(), messages);
@@ -105,7 +105,7 @@ class Schema {
   validate(source: Values, callback: ValidateCallback): Promise<Values>;
   validate(source: Values): Promise<Values>;
 
-  validate(source_: Values, o: any = {}, oc: any = () => {}): Promise<Values> {
+  validate(source_: Values, o: any = {}, oc: any = () => { }): Promise<Values> {
     let source: Values = source_;
     let options: ValidateOption = o;
     let callback: ValidateCallback = oc;
@@ -119,10 +119,10 @@ class Schema {
       }
       return Promise.resolve(source);
     }
-/**
- * 完成验证过程的方法，汇总所有验证结果并进行处理
- * @param results 验证结果数组，每个元素可以是单个验证错误或验证错误数组
- */
+    /**
+     * 完成验证过程的方法，汇总所有验证结果并进行处理
+     * @param results 验证结果数组，每个元素可以是单个验证错误或验证错误数组
+     */
     function complete(results: (ValidateError | ValidateError[])[]) {
       let errors: ValidateError[] = [];
       let fields: ValidateFieldsError = {};
@@ -342,15 +342,15 @@ class Schema {
       source,
     );
   }
-/**
- * 获取规则的类型
- * 如果规则的类型未定义，则尝试根据规则的模式将其类型设置为'pattern'
- * 此外，如果规则的验证器不是函数，并且规则的类型已定义，但不在validators中，
- * 则抛出错误表示未知的规则类型
- * 
- * @param rule 内部规则项，包含类型和模式等信息
- * @returns 返回规则的类型，如果未定义则默认为'string'
- */
+  /**
+   * 获取规则的类型
+   * 如果规则的类型未定义，则尝试根据规则的模式将其类型设置为'pattern'
+   * 此外，如果规则的验证器不是函数，并且规则的类型已定义，但不在validators中，
+   * 则抛出错误表示未知的规则类型
+   * 
+   * @param rule 内部规则项，包含类型和模式等信息
+   * @returns 返回规则的类型，如果未定义则默认为'string'
+   */
   getType(rule: InternalRuleItem) {
     if (rule.type === undefined && rule.pattern instanceof RegExp) {
       rule.type = 'pattern';
@@ -364,13 +364,13 @@ class Schema {
     }
     return rule.type || 'string';
   }
-/**
- * 获取验证方法
- * 根据给定的验证规则项，动态确定使用哪个验证函数
- * 
- * @param rule 验证规则项，包含了验证逻辑和可能的自定义验证函数
- * @returns 返回找到的验证函数，如果没有匹配的验证函数则返回undefined
- */
+  /**
+   * 获取验证方法
+   * 根据给定的验证规则项，动态确定使用哪个验证函数
+   * 
+   * @param rule 验证规则项，包含了验证逻辑和可能的自定义验证函数
+   * @returns 返回找到的验证函数，如果没有匹配的验证函数则返回undefined
+   */
   getValidationMethod(rule: InternalRuleItem) {
     if (typeof rule.validator === 'function') {
       return rule.validator;
